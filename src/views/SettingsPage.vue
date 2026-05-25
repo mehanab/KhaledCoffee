d05e740752df203b6f59245afbe958a267cfda44
Mehana <mehana@answeb.net>
2026-05-25 21:02:31 +0200

    Ajout des pages History et Settings avec leur structure de base, incluant les composants IonPage et IonContent.

---
+++ src/views/SettingsPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <ion-page>
+    <ion-content>
+      <h1>Settings</h1>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+  import { IonPage, IonContent } from '@ionic/vue';
+</script>
