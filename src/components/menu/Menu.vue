232a15394a932cb694ae4988da8b1129a095910e
Mehana <mehana@answeb.net>
2026-05-25 21:58:54 +0200

    WIP: Ajout du composant Menu avec intégration dans MenuPage et mise à jour de la structure de navigation.

---
+++ src/components/menu/Menu.vue
@@ -0,0 +1,13 @@
+<template>
+		<ion-header :translucent="false">
+            <Toolbar :title="'Menu'" :showMenuButton="false" />
+		</ion-header>
+
+		<ion-content :fullscreen="true">
+        </ion-content>
+</template>
+
+<script setup lang="ts">
+    import { IonHeader, IonContent } from '@ionic/vue';
+    import Toolbar from '../Toolbar.vue';
+</script>
--- src/views/MenuPage.vue
+++ src/views/MenuPage.vue
@@ -1,11 +1,21 @@
 <template>
-  <ion-page>
-    <ion-content>
-      <h1>Menu</h1>
-    </ion-content>
-  </ion-page>
+	<ion-page>
+		<ion-page>
+			<ion-nav ref="nav" :root="component"></ion-nav>
+		</ion-page>
+	</ion-page>
 </template>
 
 <script setup lang="ts">
-  import { IonPage, IonContent } from '@ionic/vue';
+  import { markRaw } from 'vue';
+  import { IonNav, IonPage } from '@ionic/vue';
+  import Menu from '../components/menu/Menu.vue';
+  import { ref, provide } from 'vue';
+
+  const component = markRaw(Menu);
+  const nav = ref<InstanceType<typeof IonNav>>();
+  provide('ionMenuNav', nav);
 </script>
+
+<style scoped>
+</style>
