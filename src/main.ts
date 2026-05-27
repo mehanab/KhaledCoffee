e7056b996a2c0f7b2a9ce0a32a3db2c45245fcc0
Mehana <mehana@answeb.net>
2026-05-27 23:24:41 +0200

    Feat: Ajout du fichier app.css et extraction des styles de badge et d'icône depuis variables.css

--- src/main.ts
+++ src/main.ts
@@ -34,6 +34,7 @@ import '@ionic/vue/css/palettes/dark.system.css';
 
 /* Theme variables */
 import './theme/variables.css';
+import './theme/app.css';
 
 const pinia = createPinia()
 const app = createApp(App)
